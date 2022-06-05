3f73d34c72355372f47aa5a768dd742d24b6d97c
Bernardo <bdutralemos@gmail.com>
2022-06-05 11:53:20 -0300

    Classes OrderItem and Product

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,17 @@
+namespace Desafio.Entities{
+    class OrderItem{
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+        public OrderItem(){}
+        public OrderItem(string name, Product product){
+            Name = name;
+            Price = product.price;
+            Product = product;
+        }
+
+        public double SubTotal(){
+            return Price * Quantity;
+        }
+    }
+}
